fea7f6783bf17923cafc8b3ed92083061e76787d
yudhaputraa <elekronikayudha@gmail.com>
2022-09-08 05:34:23 +0700

    belajar first

---
+++ belajar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    // inisialisasi variabel variabel
+    int a,b;
+    int hasil;
+    a = 5;
+    b = 2;
+    a = a + 1;
+    hasil = a-b;
+    // cetak hasilnya
+    cout<<hasil;
+    // menghentikan program
+    return 0;
+}
