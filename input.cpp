ac7687ee29b383ddf7b9cf938ca1629545729fd7
yudhaputraa <elekronikayudha@gmail.com>
2022-09-08 05:39:35 +0700

    belajar statemen input

---
+++ input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    a = 10;
+    cout<< a << endl;
+
+    int b;
+    cout << "masukkan nilai :";
+    cin >> b;
+    cout << "nilai yg anda masukan adalah: ";
+    cout << b << endl;
+
+    cin.get();
+    return 0;
+}
