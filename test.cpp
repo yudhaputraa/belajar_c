aec494481af028035ba904bee291a185beaa562e
yudhaputraa <elekronikayudha@gmail.com>
2022-09-08 05:34:38 +0700

    test

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+//Merupakan tipe data yang digunakan untuk menentukan benar (true) atau salah (false).
+int main(){
+	int a,b,c;
+	a = 10;
+	b = 10;
+	c = a + b;
+	cout<<"a tambah b = "<< c;
+}
+//Tipe data wchar_t biasa digunakan untuk penggunaan karakter yang besar 
