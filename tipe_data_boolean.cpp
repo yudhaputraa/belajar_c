c37b3f25151ca2151a46874b422b09899ed2d7aa
yudhaputraa <elekronikayudha@gmail.com>
2022-09-08 05:35:30 +0700

    belajar tipe data boolean

---
+++ tipe_data_boolean.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+// Merupakan tipe data yang digunakan untuk menentukan benar (true) atau salah (false).
+int main(){
+	bool t = true;
+	bool m = false;
+	
+	cout<<"Kondisi benar =  "<<t<<endl;
+	cout<<"Kondisi salah =  "<<m<<endl;
+}
+// Tipe data wchar_t biasa digunakan untuk penggunaaan karakter yang besar seperti bahasa china.
