6b3ae35b4f88690953d98614c15c369f459cd5de
yudhaputraa <elekronikayudha@gmail.com>
2022-09-08 17:23:00 +0700

    belajar if

---
+++ if.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "masukan angka = ";
+    cin >> a;
+
+    // if statemen
+    // kondisi dalam bentuk boolean
+    // jika kondisi bernilai true maka isi statemen akan dijalankan
+    if( a == 3) //ini kondisi
+    {
+        // isis statemen
+        cout << "Halooo" << endl;
+    }
+
+    cout << "Selesai" << endl;
+
+    return 0;
+}
