ac9a499449339aa2323b991e328dc3ad389f468f
yudhaputraa <elekronikayudha@gmail.com>
2022-09-09 18:26:38 +0700

    belajar dowhile

---
+++ dowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1;
+
+    do
+    {
+        cout<< "hore " << a << endl;
+        a++;
+    } while (a <= 10);
+    
+    return 0;
+}
