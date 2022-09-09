807b100a741dc7caf737624ade17c3473db2161e
yudhaputraa <elekronikayudha@gmail.com>
2022-09-09 18:14:50 +0700

    belajar while

---
+++ latwhile.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    while (a < 10)
+    {
+        cout << "hore";
+        cout<< a << endl;
+        a ++;
+    }
+
+    cout<<"selesai"<<endl;
+    
+    return 0;
+}
